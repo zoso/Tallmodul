7d6ef0d6b1f18e8728be86694b167b792ccb6465
Zoso <larslund@gmail.com>
2013-03-27 23:43:09 +0100

    parsing and setting classes on td-values

---
+++ data/data1.js
@@ -0,0 +1,27 @@
+[
+	{
+		"col1": {"txt": "Ted1", "class": "right"},
+		"col2": {"txt": "Hurra1", "class": "red"},
+		"col3": {"txt": "Balla1", "class": "bold center"},
+		"col4": {"txt": "Znøft1", "class": "right"}
+	},
+	{
+		"col1": {"txt": "Frank2", "class": "bold"},
+		"col2": {"txt": "Hurra2", "class": "red"},
+		"col3": {"txt": "Balla2", "class": "bold red"}
+	},
+	{
+		"col1": {"txt": "Joan3", "class": "red"},
+		"col2": {"txt": "Hurra3", "class": "red right"},
+		"col3": {"txt": "Balla3", "class": "bold"},
+		"col4": {"txt": "Znøft3", "class": ""},
+		"col5": {"txt": "Fuck3", "class": ""}
+	},
+	{
+		"col1": {"txt": "Joan4", "class": "red"},
+		"col2": {"txt": "Hurra4", "class": "red right"},
+		"col3": {"txt": "Balla4", "class": "bold"},
+		"col4": {"txt": "Znøft4", "class": ""},
+		"col5": {"txt": "Fuck4", "class": ""}
+	}
+]
---
+++ data/data2.js
@@ -0,0 +1,55 @@
+[
+	{
+		"col1": {"txt": "100", "class": "right"},
+		"col2": {"txt": "200", "class": "red"},
+		"col3": {"txt": "20 000", "class": "bold center"},
+		"col4": {"txt": "20 300", "class": "right"}
+	},
+	{
+		"col1": {"txt": "25425", "class": "bold"},
+		"col2": {"txt": "55636 3 6", "class": "red"},
+		"col3": {"txt": "242 24 42", "class": "bold red"}
+	},
+	{
+		"col1": {"txt": "856847564", "class": "red"},
+		"col2": {"txt": "97657856", "class": "red right"},
+		"col3": {"txt": "17456 6 ", "class": "bold"},
+		"col4": {"txt": "4y24y4", "class": ""},
+		"col5": {"txt": "4343534", "class": ""}
+	},
+	{
+		"col1": {"txt": "2434t", "class": "red"},
+		"col2": {"txt": "243463", "class": "red right"},
+		"col3": {"txt": "24536363", "class": "bold"},
+		"col4": {"txt": "34724725", "class": ""},
+		"col5": {"txt": "33246234", "class": ""}
+	},
+	{
+		"col1": {"txt": "2434t", "class": "red"},
+		"col2": {"txt": "243463", "class": "red right"},
+		"col3": {"txt": "24536363", "class": "bold"},
+		"col4": {"txt": "34724725", "class": ""},
+		"col5": {"txt": "33246234", "class": ""}
+	},
+	{
+		"col1": {"txt": "2434t", "class": "red"},
+		"col2": {"txt": "243463", "class": "red right"},
+		"col3": {"txt": "24536363", "class": "bold"},
+		"col4": {"txt": "34724725", "class": ""},
+		"col5": {"txt": "33246234", "class": ""}
+	},
+	{
+		"col1": {"txt": "2434t", "class": "red"},
+		"col2": {"txt": "243463", "class": "red right"},
+		"col3": {"txt": "24536363", "class": "bold"},
+		"col4": {"txt": "34724725", "class": ""},
+		"col5": {"txt": "33246234", "class": ""}
+	},
+	{
+		"col1": {"txt": "2434t", "class": "red"},
+		"col2": {"txt": "243463", "class": "red right"},
+		"col3": {"txt": "24536363", "class": "bold"},
+		"col4": {"txt": "34724725", "class": ""},
+		"col5": {"txt": "33246234", "class": ""}
+	}
+]
